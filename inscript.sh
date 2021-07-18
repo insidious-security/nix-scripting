144c0a1e77b5cdef010ac40bbf4d1819af8962ea
sidious <25933899+insidious-security@users.noreply.github.com>
2021-07-18 23:28:40 +0200

    Update inscript.sh

--- inscript.sh
+++ inscript.sh
@@ -21,13 +21,28 @@ printf "\n"
 printf "Version 1.0 \n"
 printf "\n"
 
+
+error(){
+	echo >&2 "$(tput bold; tput setaf 1)[-] ERROR: ${*}$(tput sgr0)"
+	exit 1
+}
+
+
 msg(){
-  echo "$(tput bold; tput setaf 2)[+] ${*}$(tput sgr0)"
+	echo "$(tput bold; tput setaf 2)[+] ${*}$(tput sgr0)"
+}
+
+
+rootpriv(){
+	if [ "${USER}" != "root" ]; then
+		error "You need to be root.. Exiting.."
+		exit 1
+	fi
 }
 
 
 install_pkgs(){
-    if pacman -Qi $1 &> /dev/null; then msg "$1 is already installed.."; else msg "Installing $1.." && sudo pacman -S --noconfirm --needed $1 > /dev/null 2>&1; fi
+	if pacman -Qi $1 &> /dev/null; then msg "$1 is already installed.."; else msg "Installing $1.." && sudo pacman -S --noconfirm --needed $1 > /dev/null 2>&1; fi
 }
 
 
@@ -40,6 +55,8 @@ vim
 lxappearance
 )
 
+
+rootpriv
 for i in "${pkgs[@]}"; do
-    install_pkgs $i
+	install_pkgs $i
 done
