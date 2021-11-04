089ce861450d3821c758e56116e86042b783ed11
sidious <25933899+insidious-security@users.noreply.github.com>
2021-11-04 21:43:48 +0100

    Update easyssh.sh

--- easyssh.sh
+++ easyssh.sh
@@ -58,7 +58,7 @@ sshdcop(){
     MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
 EOT
     #systemctl restart sshd.service && systemctl status sshd.service > /dev/null 2>&1  
-    /etc/init.d/sshd.service restart 
+    /etc/init.d/ssh restart 
     
     if [ $? -eq 0 ]; then
         msg "SSH configuration is done."
